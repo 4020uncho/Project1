96b5db497e1b835c25f218a533dd26299bac39b4
4020uncho <uncholimbu5@gmail.com>
2026-02-24 22:22:59 +0545

    Fixed the margins and fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css"
 import {BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./Pages/home/home"
+
 function App() {
   
 
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 
 const Home = () => {
   return (
@@ -8,6 +6,7 @@ const Home = () => {
      <h1>Institute of science and technology</h1>  
      <h2>Department of IT</h2>
      <h3>Central campus of Technology</h3>
+     <h4>Dharan-14, Sunsari </h4>
     </div>
     </header>
     
