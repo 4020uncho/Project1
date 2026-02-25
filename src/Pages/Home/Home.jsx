f5a445c1b8e5fa30719dcf7d7d63c7598bc716d2
4020uncho <uncholimbu5@gmail.com>
2026-02-25 20:27:02 +0545

    Added logo

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,10 @@
+import reactLogo from "../../assets/logo.png";
 
 const Home = () => {
   return (
     <header className="header">
     <div>
+      <img src={reactLogo} alt="React Logo" width="200" />
      <h1>Institute of science and technology</h1>  
      <h2>Department of IT</h2>
      <h3>Central campus of Technology</h3>
