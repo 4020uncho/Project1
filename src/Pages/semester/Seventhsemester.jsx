7b46a2b77b994d4fb9688b7a55e2a8c39b17e6b7
4020uncho <uncholimbu5@gmail.com>
2026-03-23 00:18:16 +0545

    Integrated the footer field

--- src/Pages/semester/Seventhsemester.jsx
+++ src/Pages/semester/Seventhsemester.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import image from "../../assets/semesters/sem7.png"
 import Navbar from '../../components/Navbar/Navbar'
 import Nav1 from '../../components/coursenav/Nav1'
+import Footer from '../../components/footer/Footer'
 
 const Seventhsemester = () => {
   return (
@@ -15,6 +16,7 @@ const Seventhsemester = () => {
         style={{ width: '100%', height: 'auto' }}
       />
     </div>
+    <Footer/>
     </div>
   )
 }
