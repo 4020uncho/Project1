e2c2a041aecb6ff2ca8e64734e4574fb4f5836ef
4020uncho <uncholimbu5@gmail.com>
2026-04-07 11:24:49 +0545

    Added the new feature , attendance grid allowing user to track the attendance

--- src/Pages/Attendance/Attendance.jsx
+++ src/Pages/Attendance/Attendance.jsx
@@ -1,58 +1,51 @@
 import React from "react";
 import "./Attendance.css";
 
-
-
 const Attendance = () => {
+  const days = Array.from({ length: 30 }, (_, i) => i + 1);
+   const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+
   return (
-         <div className="container">
-      <h1>Attendance Tracker</h1>
-      <p className="subtitle">
-        Detailed view of your participation metrics for the current term.
-      </p>
+   <div className="container">
+      <h2>Attendance for {months[new Date().getMonth()]} {new Date().getFullYear()}</h2>
+    <div className="table-wrapper">
+      <table className="attendance-table">
+        <thead>
+          <tr>
+            {days.map((day) => (
+              <th key={day}>{day}</th>
+            ))}
+            <th>Present</th>
+            <th>Absent</th>
+          </tr>
+        </thead>
 
-      {/* Summary */}
-      <div className="summary-card">
-        <div className="progress">
-          <div className="circle">88%</div>
-        </div>
-
-        <div className="status">
-          <p>STATUS</p>
-          <h3>Term Records</h3>
-        </div>
-
-        <div className="stats">
-          <div>
-            <h2>42</h2>
-            <p>PRESENT</p>
-          </div>
-          <div>
-            <h2 className="red">2</h2>
-            <p>UNEXCUSED</p>
-          </div>
-          <div>
-            <h2 className="orange">4</h2>
-            <p>LATE</p>
-          </div>
-        </div>
+        <tbody>
+          <tr>
+            {days.map((day) => (
+              <td key={day}></td>
+            ))}
+            <td>0</td>
+            <td>0</td>
+          </tr>
+        </tbody>
+      </table>
       </div>
-
-      {/* Calendar */}
-      <div className="calendar-card">
-        <div className="calendar-header">
-          <h3>September 2024</h3>
-          <div className="legend">
-            <span className="dot blue"></span> Present
-            <span className="dot red"></span> Absent
-            <span className="dot orange"></span> Late
-          </div>
-        </div>
-
-       
-
       </div>
-    </div>
   );
 };
 
---
+++ src/components/dropdown/Dropdown.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Dropdown = () => {
+    const month = [
+    "January",
+    "February",
+    "March",    
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"  
+    ]
+  return (
+    <div>
+         
+    </div>
+  )
+}
+
+export default Dropdown
